5c19954b6cd9d46204cfc74a6434643443b6e124
yonatangross <yonatan2gross@gmail.com>
2020-09-01 14:37:29 +0300

    Add project files.

---
+++ Contracts/OpeningHours.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GamingStore.Contracts
+{
+    [NotMapped]
+    public class OpeningHours
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+        public DateTime OpeningTime { get; set; }
+        public DateTime ClosingTime { get; set; }
+    }
+}
