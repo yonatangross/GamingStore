a552b9bcfd09f2839c8792b8c3331fbd1c3bee9d
yonatangross <yonatan2gross@gmail.com>
2020-09-06 14:47:02 +0300

    added many to many relationships instead of dictionaries.

---
+++ Models/Relationships/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GamingStore.Models.Relationships
+{
+    public class OrderItem
+    {
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public Order Order { get; set; }
+        public Item Item { get; set; }
+        public uint ItemsCount { get; set; }
+    }
+}
