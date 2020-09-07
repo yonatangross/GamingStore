7c8e5b2efbae36dda3f88db6fe28f1d78da8c74f
Ohad Cohen <ohad338@gmail.com>
2020-09-07 04:48:32 +0300

    added more payments/customer/items with some new complex objects + added more store data

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -23,31 +23,31 @@ namespace GamingStore.Data
             //TODO: add different items with properties
             var items = new Item[]
             {
-                new Item{Title="Keyboard",Manufacturer= "Microsoft",Price = 299,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Keyboard",Manufacturer= "Microsoft",Price = 299,Category = "hardware",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }},
-                new Item{Title="Mouse",Manufacturer= "Microsoft",Price = 150,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Mouse",Manufacturer= "Microsoft",Price = 150,Category = "hardware",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }},
-                new Item{Title="Gaming Chair",Manufacturer= "Razor",Price=799,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Gaming Chair",Manufacturer= "Razor",Price=799,Category = "other",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }},
-                new Item{Title="Mouse Pad",Manufacturer= "Razor",Price=50,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Mouse Pad",Manufacturer= "Razor",Price=50,Category = "gear",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }},
-                new Item{Title="Graphic Card",Manufacturer= "Nvidia",Price = 1500,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Graphic Card",Manufacturer= "Nvidia",Price = 1500,Category = "GPUs hardware",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }},
-                new Item{Title="Processor",Manufacturer= "Intel",Price = 1249,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Processor",Manufacturer= "Intel",Price = 1249,Category = "CPU ",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }},
-                new Item{Title="Headphones",Manufacturer= "Bose",Price = 499,PropertiesList =new Dictionary<string, string>()  {
+                new Item{Title="Headphones",Manufacturer= "Bose",Price = 499,Category = "hardware",PropertiesList =new Dictionary<string, string>()  {
                     {"diameter", "3"},
                     { "width", "15"},
                     { "height", "4"} }}
@@ -197,6 +197,14 @@ namespace GamingStore.Data
             //todo: add payments with all parameters.
             var payments = new Payment[]
             {
+                new Payment{OrderId =1, ItemsCost = 543,ShippingCost = 20,Paid = true,PaymentMethod=PaymentMethod.Paypal},
+                new Payment{OrderId =2, ItemsCost = 250,ShippingCost = 20,Paid = true,PaymentMethod=PaymentMethod.Cash},
+                new Payment{OrderId =3, ItemsCost = 6043,ShippingCost = 0,Paid = false,PaymentMethod=PaymentMethod.CreditCard},
+                new Payment{OrderId =4, ItemsCost = 300,ShippingCost = 30,Paid = true,PaymentMethod=PaymentMethod.Cash},
+                new Payment{OrderId =5, ItemsCost = 987,ShippingCost = 30,Paid = false,PaymentMethod=PaymentMethod.CreditCard},
+                new Payment{OrderId =6, ItemsCost = 100,ShippingCost = 50,Paid = true,PaymentMethod=PaymentMethod.Cash}
+
+
             };
             foreach (var p in payments)
             {
@@ -207,16 +215,46 @@ namespace GamingStore.Data
             //todo: add customers with all parameters including complex objects.
             var customers = new Customer[]
             {
-            new Customer{FirstName="Carson",LastName="Alexander",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474",OrderHistory = new List<Order>(){}},
-            new Customer{FirstName="Meredith",LastName="Alonso",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
-            new Customer{FirstName="Arturo",LastName="Anand",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
-            new Customer{FirstName="Gytis",LastName="Barzdukas",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Carson",LastName="Alexander",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474",OrderHistory = new List<Order>(){}
+                ,Address=new Address(){ Address1 = "Shimon Peres 22" ,
+                    City = "Holon",
+                    Address2 = "Shimon Peres 22 Apartment 3",
+                    Country = "Israel",
+                    PostalCode = "463722",
+                    State ="Shipped"} },
+            new Customer{FirstName="Meredith",LastName="Alonso",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474",OrderHistory = new List<Order>(){}
+                ,Address=new Address(){ Address1 = "Shimon Peres 10" ,
+                    City = "Rishon Lezion",
+                    Address2 = "Shimon Peres 10 Apartment 4",
+                    Country = "Israel",
+                    PostalCode = "132134",
+                    State ="On the way"} },
+            new Customer{FirstName="Arturo",LastName="Anand",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474",OrderHistory = new List<Order>(){}
+                ,Address=new Address(){ Address1 = "Shimon 15" ,
+                    City = "Bat-Yam",
+                    Address2 = "Shimon 15 Apartment 20",
+                    Country = "Israel",
+                    PostalCode = "203941",
+                    State ="Delivered"} },
+            new Customer{FirstName="Gytis",LastName="Barzdukas",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474",OrderHistory = new List<Order>(){}
+                ,Address=new Address(){ Address1 = "Peres 53" ,
+                    City = "Hedera",
+                    Address2 = "Peres 22 Apartment 53",
+                    Country = "Israel",
+                    PostalCode = "549382",
+                    State ="Shipped"} },
             new Customer{FirstName="Yan",LastName="Li",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
             new Customer{FirstName="Peggy",LastName="Justice",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
-            new Customer{FirstName="Peggy",LastName="Justice",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Peggy",LastName="Aniston",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
             new Customer{FirstName="Laura",LastName="Norman",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
             new Customer{FirstName="Nino",LastName="Olivetto",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
-            new Customer{FirstName="Ohad",LastName="Bolilon",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"}
+            new Customer{FirstName="Ohad",LastName="Bolilon",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Matan",LastName="Bolilon",Email = "bolilon@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Almog",LastName="Tfi",Email = "anubis@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Aviv",LastName="Bolila",Email = "Bil954@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Rick",LastName="Holio",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Matan",LastName="Hamor",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Customer{FirstName="Yoni",LastName="Bolilon",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"}
             };
             foreach (var c in customers)
             {
@@ -234,7 +272,15 @@ namespace GamingStore.Data
             new Store{Id=4,Name="Shimi Gaming Chairs",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
             new Store{Id=5,Name="Ohad GamingWorld",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
             new Store{Id=6,Name="Miranda Mouses",Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
-            new Store{Id=7,Name="Yoni Keyboards" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"}
+            new Store{Id=7,Name="Yoni Keyboards" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=8,Name="Yoni Headphones" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=9,Name="Matan GPUs " ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=10,Name="GamingStore" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=11,Name="KeyboardStore" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=12,Name=" MouseStore" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=13,Name="Alam" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=14,Name="Mahsany Hashmal" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
+            new Store{Id=15,Name="BolilonGamingStore" ,Email = "yonatan2gross@gmail.com",PhoneNumber = "0506656474"},
             };
             foreach (var s in stores)
             {
