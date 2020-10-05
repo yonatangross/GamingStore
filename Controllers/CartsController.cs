077dc4b688b4ffe94bd4549f2ef4591145ec6424
Aviv Miranda <Aviv943@gmail.com>
2020-10-05 04:04:48 +0300

    ItemView: -removed an access to 'add to cart' button for unauthorized customers (not registered)
    -CartView:
    -removed an access to the cotroller for unauthorized customers (not registered)

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GamingStore.Data;
 using GamingStore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -27,12 +28,13 @@ namespace GamingStore.Controllers
 
         private Task<Customer> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
+        [Authorize]
         public async Task<IActionResult> Index()
         {
             try
             {
                 Customer customer = await GetCurrentUserAsync();
-                var itemsInCart = _context.Carts.Where(c => c.CustomerId == customer.Id);
+                IQueryable<Cart> itemsInCart = _context.Carts.Where(c => c.CustomerId == customer.Id);
 
                 foreach (Cart cartItem in itemsInCart)
                 {
@@ -47,53 +49,12 @@ namespace GamingStore.Controllers
             {
                 
             }
-            
-            //try
-            //{
-            //    foreach ((int key, uint value) in cart.ShoppingCart)
-            //    {
-            //        Item item = _context.Items.First(i => i.Id == key);
-            //        cart.ItemsInCart.Add(item, value);
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-                
-            //}
-
-            //var list = new List<ItemsInCart>();
-
-            //foreach (KeyValuePair<int, uint> item in cart.ShoppingCart)
-            //{
-            //    foreach (Item contextItem in _context.Items)
-            //    {
-            //        if (contextItem.Id != item.Value)
-            //        {
-            //            continue;
-            //        }
-
-            //        var itemInCart = new ItemsInCart()
-            //        {
-            //            Price = contextItem.Price,
-            //            ItemId = contextItem.Id,
-            //            Quantity = item.Value,
-            //            Title = contextItem.Title
-            //        };
-
-            //        list.Add(itemInCart);
-            //        break;
-            //    }
-            //}
 
             return View();
         }
 
-        private bool CartExists(string id)
-        {
-            return _context.Carts.Any(e => e.CustomerId == id);
-        }
-
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> Delete()
         {
             Customer customer = await GetCurrentUserAsync();
