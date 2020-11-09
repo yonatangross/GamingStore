f6e272452cdb38e06eab44c707d87dd282672021
Matan Hassin <matan18061806@gmail.com>
2020-11-09 02:35:40 +0200

    added view model

---
+++ src/GamingStore/ViewModels/RelatedItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamingStore.Models;
+
+namespace GamingStore.ViewModels
+{
+    public class RelatedItemViewModel
+    {
+        public RelatedItemViewModel()
+        {
+
+        }
+        public int RelatedItemId { get; set; }
+        
+        public List<RelatedItem> RelatedItem { get; set; }
+
+        public int ItemId { get; set; }
+        public List<Item> Item { get; set; }
+    }
+}
