034d346c5b16ea7b23d3b1bac67c7519f3ba2ade
Aviv Miranda <Aviv943@gmail.com>
2020-11-09 14:29:17 +0200

    added product parser

---
+++ tools/GamingStore.GetProducts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamingStore.ProductParser
+{
+    class Item
+    {
+        public string Title { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
